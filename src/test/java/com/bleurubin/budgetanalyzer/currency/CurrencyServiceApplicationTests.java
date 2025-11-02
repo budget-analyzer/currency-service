0f9e7413a20382078bf1c164ff224b65989c067f
Bleu Rubin <bleurubin@protonmail.com>
2025-11-02 10:11:08 -0500

    add back test

---
+++ src/test/java/com/bleurubin/budgetanalyzer/currency/CurrencyServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.bleurubin.budgetanalyzer.currency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}

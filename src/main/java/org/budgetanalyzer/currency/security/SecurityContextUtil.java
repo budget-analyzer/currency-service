2cd99c2d72666b8a710e403df6136fcf459dcc6d
Bleu Rubin <bleurubin@gmail.com>
2025-11-18 18:23:54 -0500

    polish

--- src/main/java/org/budgetanalyzer/currency/security/SecurityContextUtil.java
+++ src/main/java/org/budgetanalyzer/currency/security/SecurityContextUtil.java
@@ -5,9 +5,7 @@ import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
 /**
